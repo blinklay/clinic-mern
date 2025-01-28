670bcb63dfc84fb59b8459bb2bcdecd479932147
blinklay <danillitvinenkod00@gmail.com>
2025-01-29 00:40:26 +0300

    Implement a method for authorization

---
+++ controllers/user.controller.js
@@ -0,0 +1,45 @@
+const jwt = require("jsonwebtoken")
+const User = require("../models/User.model")
+const bcrypt = require("bcrypt")
+const { JWT_SECRET } = require("../constants")
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден!"
+      })
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password)
+
+    if (!isValidPassword) {
+      return res.status(400).json({
+        message: "Введен неверный пароль!"
+      })
+    }
+
+    const token = jwt.sign({
+      email: user.email,
+      id: user._id
+    }, JWT_SECRET, {
+      expiresIn: "24h"
+    })
+
+    res.cookie("token", token, { httpOnly: true })
+    res.status(200).json({
+      message: "Успешная авторищация!"
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось авторизироваться!"
+    })
+  }
+}
+
+module.exports = {
+  login
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const { login } = require("./controllers/user.controller")
+const { default: mongoose } = require("mongoose")
+const cookieParser = require("cookie-parser");
+
+const PORT = process.env.PORT || 8080
+const app = express()
+
+app.use(cookieParser());
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://admin:123qwe@cluster0.d86hc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => {
+    console.log('DataBase has been conected!');
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+app.post("/login", login)
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error("Error when starting the server:", err);
+    process.exit(1)
+  }
+
+  console.log(`Server has been start on port: ${PORT}`);
+})
